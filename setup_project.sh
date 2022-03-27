17ff16ce8fd6d495453758d0834a34ba81f7417f
AshWReaper <30325741+AshWReaper@users.noreply.github.com>
2022-03-27 03:14:31 +0200

    fixed bugs

--- setup_project.sh
+++ setup_project.sh
@@ -5,7 +5,7 @@ echo "Creating packacge.json file";
 npm init;
 
 ## get project name from user
-read -p "What should this project be called?" PROJECT_NAME;
+read -p "What should this project be called? " PROJECT_NAME;
 
 ## check node version
 NODE_VERSION=`node --version`;
@@ -42,12 +42,22 @@ mkdir production;
 mkdir production/inc/php;
 mkdir production/inc/css;
 mkdir production/inc/js;
+
 ## create index.php file in dev directory
+echo '<head></head>' > dev/index.php;
+echo '<body>' >> dev/index.php;
 echo '<?php echo "<h1>Hello, World!</h1>"; ?>' >> dev/index.php;
+echo '</body>' >> dev/index.php;
+
+## create css file
+echo '/*styles go here*/' >> dev/css/styles.css;
+
+## create js file
+echo '// scripts go here' >> dev/js/scripts.css;
 
 ## Install the gulp package in your devDependencies
 echo "installing gulp package dependencies";
-npm install --save-dev gulp;
+sudo npm install --save-dev gulp;
 
 ## Verify your gulp versions
 echo "Verifying Fulp versions";
@@ -56,17 +66,17 @@ echo "Gulp version: " $GULP_VERSION
 
 ## For PHP: add gulp-connect-php as a development dependency
 echo "Installing gulp-coonnect-php dependency";
-npm install --save-dev gulp-connect-php;
+sudo npm install --save-dev gulp-connect-php;
 
 ## For FTP: install the vinyl-ftp plugin using NPM
 echo "Installing vinyl-ftp plugin";
 sudo npm install --save-dev vinyl-ftp;
 
 ## Create a gulpfile
-echo "copy the code from here: https://github.com/AshWReaper/Gulp-Browsersync/blob/main/gulpfile.js" >> gulpfile.js;
+echo "copy the code from here: https://github.com/AshWReaper/Gulp-Browsersync/blob/main/gulpfile.js" >> dev/gulpfile.js;
 echo "A gulpfile.js file was created; Replace the text inside with code from: https://github.com/AshWReaper/Gulp-Browsersync/blob/main/gulpfile.js";
 
 ## Prompt to exit
-read -p "Press enter to exit this program and start Gulp";
+read -p "Once you've copied the contents of the gulp file then press enter to exit this program and start Gulp";
 
 gulp start;
