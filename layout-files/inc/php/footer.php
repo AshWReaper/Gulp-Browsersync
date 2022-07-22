9e6847cb74420c0dce16f91213ac3d3b5c8cf69d
AshWReaper <ashton603@gmail.com>
2022-07-22 13:13:35 +0200

    looking into adding standard layout files to use instead of bashing all the lines into place...

---
+++ layout-files/inc/php/footer.php
@@ -0,0 +1,11 @@
+</main>
+
+<footer>
+
+</footer>
+
+<!-- Include JS Scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script type="text/javascript" src="./inc/js/scripts.js"></script>
+</body>
+</html>
